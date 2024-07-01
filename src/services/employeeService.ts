7ed8e45d1d82080ccf5c9417d53bae3a01d60fb0
PauloMats <pm.mats98@gmail.com>
2024-07-01 08:43:39 -0300

    Fix and Feat: Corrige interface incorreta e implementa código de erro para API

--- src/services/employeeService.ts
+++ src/services/employeeService.ts
@@ -3,14 +3,16 @@
 export interface Employee {
     id: number;
     name: string;
-    position: string;
-    admissionDate: string;
+    job: string;
+    admission_date: string;
     phone: string;
-    photo: string;
+    image: string;
   }
   
   export const fetchEmployees = async (): Promise<Employee[]> => {
     const response = await fetch('http://localhost:5000/employees');
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
     return response.json();
   };
-  
