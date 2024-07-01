670b275727ebc3fdf8038ac69cf9f2ee3b3b0e45
PauloMats <pm.mats98@gmail.com>
2024-07-01 09:24:11 -0300

    Feat: Adiciona Logo da Be no Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Header'
+import logo from '../../assets/logobe.png'
 
 const Header: React.FC = () => {
     return (
         <header>
-            <div className='header'> ‎ ‎ ‎   </div>
+            <div className='header'> 
+                <img src={logo} alt="logotipo BeMobile" />
+                 ‎ ‎ ‎  
+                 </div>
             <h1>Funcionários</h1>
         </header>
     );
